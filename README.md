bb9140e464731593f9646246222035f73cdc0723
unknown <zhilovgithub@gmail.com>
2024-11-30 19:20:42 +0300

    2.0

--- README.md
+++ README.md
@@ -8,3 +8,6 @@
     - Amount of **Active tasks** (Running + Runnable + Uninterraptible Sleep) starts from **zero** and increases on every iteration by 60% chance and decreases on every iteration by 40% chance. 
     - Exponent is taken 0.08
     - [Exponentially weighted moving average (EWMA)](https://en.wikipedia.org/wiki/Moving_average#Exponential_moving_average) is used for calculations
+- 2.0
+    - Added bitwise operations
+    - Full copy from real Linux Sources where this [algorithm](https://github.com/torvalds/linux/blob/2ba9f676d0a2e408aef14d679984c26373bf37b7/include/linux/sched/loadavg.h#L4) is written in C
--- cmd/main.go
+++ cmd/main.go
@@ -5,35 +5,59 @@ import (
 	"math/rand"
 )
 
+const (
+	FSHIFT  = 11
+	FIXED_1 = 1 << FSHIFT
+)
+
 func main() {
 	var (
-		load   float64 = 0
-		exp    float64 = 0.08
-		active int64   = 0
+		load   uint64 = 0
+		exp    uint64 = 1884
+		active uint64 = 0
 	)
 
 	var randValue int
-	for i := 0; i < 1200; i++ {
-		randValue = rand.Intn(10)
-		if randValue >= 4 { // 60% that active tasks amount will increase
-			active++
-		} else {
-			active--
-			if active < 0 {
-				active = 0
+	for i := 0; i < 220; i++ {
+		if active != 21 { // When it reaches 21 let it be always 21
+			randValue = rand.Intn(10)
+			if randValue >= 4 { // 60% that active tasks amount will increase
+				active++
+			} else {
+				if active != 0 {
+					active--
+				}
 			}
 		}
 
-		result := calcLoad(load, exp, active)
+		result := calcLoad(load, exp, active*FIXED_1)
+		load = result
+
+		output := getLoadAverage(result, FIXED_1/200, 0)
 		if i%2 == 0 {
-			fmt.Printf("Load Average in 1min: %3.2f\tActive Tasks (R + D): %d\n", result, active)
+			fmt.Printf("Load Average in 1min: %d.%d\tActive Tasks (R + D): %d\n", loadInt(output), loadFraq(output), active)
 		}
 
-		load = result
 	}
 }
 
-func calcLoad(load, exp float64, active int64) float64 {
-	newLoad := load*exp + float64(active)*(1-exp)
-	return newLoad
+func calcLoad(load, exp uint64, active uint64) uint64 {
+	newLoad := load*exp + active*(FIXED_1-exp)
+
+	if active >= load {
+		newLoad += FIXED_1 - 1
+	}
+	return newLoad / FIXED_1
+}
+
+func getLoadAverage(load, offset uint64, shift uint64) uint64 {
+	return (load + offset) << shift
+}
+
+func loadInt(x uint64) uint64 {
+	return x >> FSHIFT
+}
+
+func loadFraq(x uint64) uint64 {
+	return loadInt(((x) & (FIXED_1 - 1)) * 100)
 }
